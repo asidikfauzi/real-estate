a5a3e99bd4803f6874bcf1ea22446d17e7dbe4a8
asidikfauzi <asidikfauzi@gmail.com>
2022-11-21 00:46:48 +0700

    create model

---
+++ app/Models/Perumahan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Perumahan extends Model
+{
+    use HasFactory;
+
+    protected $table = "perumahan";
+    protected $guarded = [];
+}
