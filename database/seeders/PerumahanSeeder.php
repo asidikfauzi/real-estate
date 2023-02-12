5e6013786cf2b053194f5f0a92b230ac2e36fd98
asidikfauzi <asidikfauzi@gmail.com>
2023-02-12 16:39:09 +0700

    fix seeder

--- database/seeders/PerumahanSeeder.php
+++ database/seeders/PerumahanSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use DB;
+use App\Models\Perumahan;
 
 class PerumahanSeeder extends Seeder
 {
@@ -15,35 +15,39 @@ class PerumahanSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('perumahans')->insert([
-            'id' => 'cb1a621f-05a5-49e6-8767-36455fcbeced',
-            'image' => 'rumahsatu.jpg',
-            'lebar' => '10',
-            'panjang' => '10',
-            'alamat' => 'Jl Lippo Jember Di Atas Genteng no 10',
-            'harga' => '150000000',
-            'keterangan' => 'Di jual tanpa perantara',
-            'status' => true,
-        ],
-        [
-            'id' => 'e67c205c-8011-4ed7-853a-4daaf15f3208',
-            'image' => 'rumahdua.jpg',
-            'lebar' => '11',
-            'panjang' => '15',
-            'alamat' => 'Jl Ozie Jember Di Atas Genteng no 11',
-            'harga' => '170000000',
-            'keterangan' => 'Di jual tanpa perantara',
-            'status' => true,
-        ],
-        [
-            'id' => '427cc453-c72f-42dd-8a44-ac89b833a728',
-            'image' => 'rumahtiga.jpg',
-            'lebar' => '10',
-            'panjang' => '20',
-            'alamat' => 'Jl Eterno Jember Di Atas Genteng no 10',
-            'harga' => '260000000',
-            'keterangan' => 'Di jual tanpa perantara',
-            'status' => false,
-        ],);
+        $properties = [
+            [
+                'id' => 'cb1a621f-05a5-49e6-8767-36455fcbeced',
+                'image' => 'rumahsatu.jpg',
+                'lebar' => '10',
+                'panjang' => '10',
+                'alamat' => 'Jl Lippo Jember Di Atas Genteng no 10',
+                'harga' => '150000000',
+                'keterangan' => 'Di jual tanpa perantara',
+                'status' => true,
+            ],
+            [
+                'id' => 'e67c205c-8011-4ed7-853a-4daaf15f3208',
+                'image' => 'rumahdua.jpg',
+                'lebar' => '11',
+                'panjang' => '15',
+                'alamat' => 'Jl Ozie Jember Di Atas Genteng no 11',
+                'harga' => '170000000',
+                'keterangan' => 'Di jual tanpa perantara',
+                'status' => true,
+            ],
+            [
+                'id' => '427cc453-c72f-42dd-8a44-ac89b833a728',
+                'image' => 'rumahtiga.jpg',
+                'lebar' => '10',
+                'panjang' => '20',
+                'alamat' => 'Jl Eterno Jember Di Atas Genteng no 10',
+                'harga' => '260000000',
+                'keterangan' => 'Di jual tanpa perantara',
+                'status' => true,
+            ]
+        ];
+
+        Perumahan::insert($properties);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use DB;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -16,12 +16,14 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
-        DB::table('users')->insert([
+        $user = [
             'id' => '1ef480f4-85fb-4898-a134-ed3f5c6ea711',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('Password123!'),
             'name' => 'admin',
             'role' => 'admin',
-        ]);
+        ];
+
+        User::insert($user);
     }
 }
