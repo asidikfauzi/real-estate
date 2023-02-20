7446db0100f33dadadea6dda8d00aec6ac82efb5
asidikfauzi <asidikfauzi@gmail.com>
2023-02-21 01:05:42 +0700

    seeder database run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             PerumahanSeeder::class,
+            AgentSeeder::class,
         ]);
     }
 }
