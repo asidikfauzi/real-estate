b8e27d68596943f476f69069131e2010457fbb67
asidikfauzi <asidikfauzi@gmail.com>
2023-03-16 14:21:05 +0700

    change seeder

--- database/seeders/PesanSeeder.php
+++ database/seeders/PesanSeeder.php
@@ -20,8 +20,8 @@ class PesanSeeder extends Seeder
                 'user_to' => '1ef480f4-85fb-4898-a134-ed3f5c6ea711', //admin
                 'keterangan' => 'Mas perumahan di lippo 097 saya ingin pesan',
                 'image' => '',
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s"),
+                'created_at' => "2023-03-14 13:43:22",
+                'updated_at' => "2023-03-14 13:43:22",
             ],
             [
                 'user_from' => '1ef480f4-85fb-4898-a134-ed3f5c6ea711',
@@ -30,16 +30,16 @@ class PesanSeeder extends Seeder
                         \n 08899087871 BCA \n atas nama KOLONEL MUKIDI
                         \n dan kirimkan syarat kemampuan ninja warior',
                 'image' => '',
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s"),
+                'created_at' => "2023-03-14 13:44:22",
+                'updated_at' => "2023-03-14 13:44:22",
             ],
             [
                 'user_from' => '922933f6-802f-44ba-a79a-d48949fe43de', //angga
                 'user_to' => '1ef480f4-85fb-4898-a134-ed3f5c6ea711', //admin
                 'keterangan' => 'Baik mas saya akan kirimkan',
                 'image' => '',
-                'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s"),
+                'created_at' => "2023-03-14 13:45:22",
+                'updated_at' => "2023-03-14 13:45:22",
             ]
         ];
 
