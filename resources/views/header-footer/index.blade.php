4c36ecac6b54a62c3e05ff0f3ccec15fdcb70b74
asidikfauzi <asidikfauzi@gmail.com>
2023-03-20 00:31:16 +0700

    clean code

--- resources/views/header-footer/index.blade.php
+++ resources/views/header-footer/index.blade.php
@@ -140,21 +140,13 @@
       </div>
     </div>
   </section>
-
-
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
   <!-- Vendor JS Files -->
   <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
   <script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
-
-
-
   <!-- Template Main JS File -->
   <script src="{{asset('assets/js/main.js')}}"></script>
-
 </body>
-
 </html>
